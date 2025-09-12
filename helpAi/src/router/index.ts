7c855b7e8e0dd87fbad322b4a1fff411902fbb5e
Your Name <your.email@example.com>
2025-09-11 22:09:07 -0300

    QB-33-feat(router): created reouter to add views

--- helpAi/src/router/index.ts
+++ helpAi/src/router/index.ts
@@ -2,7 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('../views/DashboardView.vue')
+    },
+  ],
 })
 
 export default router
