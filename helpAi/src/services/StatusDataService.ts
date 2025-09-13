644f1931301797814590def859d8526ac5331d59
Your Name <your.email@example.com>
2025-09-12 22:19:18 -0300

    QB-31(fix):Rename ticket-status to status_name

--- helpAi/src/services/StatusDataService.ts
+++ helpAi/src/services/StatusDataService.ts
@@ -1,9 +1,8 @@
 import { api } from "./apiConfig";
+import type { AxiosResponse } from "axios";
 
-import  type  { AxiosResponse } from "axios";
-
-interface Status{
-  ticket_status: string;
+interface Status {
+  status_name: string; 
   ticket_count: number;
 }
 
@@ -11,10 +10,10 @@ interface StatusesResponse {
   data: Status[];
 }
 
-export class StatusDataService{
-  async getStatusData(): Promise<Status[]>{
+export class StatusDataService {
+  async getStatusData(): Promise<Status[]> {
     try {
-      const response: AxiosResponse<StatusesResponse> = await api.get<StatusesResponse>("/tickets/tickets-by-status")
+      const response: AxiosResponse<StatusesResponse> = await api.get<StatusesResponse>("/tickets/tickets-by-status");
       return response.data.data;
     } catch (error) {
       console.error("Error fetching status data:", error);
