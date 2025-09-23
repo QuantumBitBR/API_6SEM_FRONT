aa07710ac29acb3cbf63f52d04032dc8ee1bcc27
André Meneses <andremeneses0103@gmail.com>
2025-09-22 21:59:02 -0300

    QB-48-style(grid): added grid display to organize charts in Dashboard

--- helpAi/src/views/DashboardView.vue
+++ helpAi/src/views/DashboardView.vue
@@ -1,30 +1,45 @@
 <template>
   <div>
-    <div>
-      <TicketsByStatus/>
-      <TicketsPerCompany/>
-      <TicketsPerProduct/>
-      <TicketsPerCategory/>
+    <div class="grid-container1">
+      <TicketsByStatus />
+      <TicketsPerCompany />
+    </div>
+    <div class="grid-container2">
+      <TicketsPerProduct />
+      <TicketsPerCategory />
     </div>
   </div>
 </template>
 <script lang="ts">
-
-import TicketsByStatus from '@/components/TicketsByStatus.vue';
-import TicketsPerProduct from '@/components/TicketsPerProduct.vue';
-import TicketsPerCompany from '../components/TicketsPerCompany.vue';
-import TicketsPerCategory from '@/components/TicketsPerCategory.vue';
-  export default {
-    components:{
-      TicketsByStatus,
-      TicketsPerCompany,
-      TicketsPerCategory,
-      TicketsPerProduct
-    }
-
-  }
+import TicketsByStatus from '@/components/TicketsByStatus.vue'
+import TicketsPerProduct from '@/components/TicketsPerProduct.vue'
+import TicketsPerCompany from '../components/TicketsPerCompany.vue'
+import TicketsPerCategory from '@/components/TicketsPerCategory.vue'
+export default {
+  components: {
+    TicketsByStatus,
+    TicketsPerCompany,
+    TicketsPerCategory,
+    TicketsPerProduct,
+  },
+}
 </script>
 <style lang="css">
 
-</style>
+.grid-container1 {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 10px;
+  width: 100%;
+  padding: 20px 0 0;
+}
 
+.grid-container2 {
+  display: grid;
+  grid-template-columns: 1fr 1.2fr;
+  gap: 10px;
+  width: 100%;
+  height: 20px;
+  padding: 20px 0 0 0;
+}
+</style>
