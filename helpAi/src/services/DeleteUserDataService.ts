1794d6268dcfcd333600e09062b55071c5f1e7c2
Gabriel Mota <gabmota8@gmail.com>
2025-09-23 22:43:17 -0300

    QB-43-feat(Delete):Created a service that deletes a user from a company by ID

---
+++ helpAi/src/services/DeleteUserDataService.ts
@@ -0,0 +1,18 @@
+import { api } from "@/services/apiConfig";
+import type { AxiosResponse } from "axios";
+
+interface DeleteUserResponse {
+ user_id: number;
+}
+
+export class DeleteUsersDataService {
+  async deleteUser(userId: number): Promise<DeleteUserResponse | null> {
+    try {
+      const response: AxiosResponse<DeleteUserResponse> = await api.delete<DeleteUserResponse>(`/delete/${userId}`);
+      return response.data;
+    } catch (error) {
+      console.error("Error deleting user:", error);
+      return null;
+    }
+  }
+}
