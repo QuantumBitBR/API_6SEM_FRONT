2c7133644899f9a46430f42a3c96fea8f690727f
André Meneses <andremeneses0103@gmail.com>
2025-10-09 21:38:26 -0300

    QB-72-feat(AllCompanies): improved Company service to get all companies

--- helpAi/src/services/CompanyDataService.ts
+++ helpAi/src/services/CompanyDataService.ts
@@ -1,22 +1,39 @@
 import { api } from "@/services/apiConfig";
 import type { AxiosResponse } from "axios";
 
-interface Company{
+interface Company {
     company_name: string;
     ticket_count: number;
+}
+interface CompanyInfo {
+    CompanyID: number;
+    CompanyName: string;
 }
 
 interface CompaniesResponse {
     data: Company[];
 }
+interface CompaniesInfoResponse {
+    data: CompanyInfo[];
+}
 
-export class CompanyDataService{
-    async getCompanyData(): Promise<Company[]>{
+export class CompanyDataService {
+    async getCompanyData(): Promise<Company[]> {
         try {
             const response: AxiosResponse<CompaniesResponse> = await api.get<CompaniesResponse>("/tickets/tickets-by-company")
             return response.data.data;
         } catch (error) {
             console.error("Error fetching company data:", error);
+            return [];
+        }
+    }
+
+    async getAllCompanies(): Promise<CompanyInfo[]> {
+        try {
+            const response: AxiosResponse<CompaniesInfoResponse> = await api.get<CompaniesInfoResponse>("/companies/all-companies");
+            return response.data.data;
+        } catch (error) {
+            console.error("Error fetching all companies:", error);
             return [];
         }
     }
