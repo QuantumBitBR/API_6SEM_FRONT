d53fda0c614e0c9cfcbec9c5c409ae2dcb911f24
André Meneses <andremeneses0103@gmail.com>
2025-11-03 22:33:07 -0300

    QB-91-fix(calendar): changed calendar language to Portuguese

--- helpAi/src/main.ts
+++ helpAi/src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import PrimeVue from 'primevue/config';
-import ToastService from 'primevue/toastservice';
-import Aura from '@primeuix/themes/aura';
-import './main.css';
+import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
+import Aura from '@primeuix/themes/aura'
+import './main.css'
 import 'primeicons/primeicons.css'
 import App from './App.vue'
 import router from './router'
@@ -15,13 +15,59 @@ app.use(PrimeVue, {
     theme: {
         preset: Aura,
         options: {
-            darkModeSelector: false
-        }
-    }
-});
+            darkModeSelector: false,
+        },
+    },
+})
 
 app.use(router)
 app.use(ToastService)
 
-
 app.mount('#app')
+
+app.config.globalProperties.$primevue.config.locale = {
+    firstDayOfWeek: 0,
+    dayNames: [
+        'domingo',
+        'segunda-feira',
+        'terça-feira',
+        'quarta-feira',
+        'quinta-feira',
+        'sexta-feira',
+        'sábado',
+    ],
+    dayNamesShort: ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sáb'],
+    dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+    monthNames: [
+        'Janeiro',
+        'Fevereiro',
+        'Março',
+        'Abril',
+        'Maio',
+        'Junho',
+        'Julho',
+        'Agosto',
+        'Setembro',
+        'Outubro',
+        'Novembro',
+        'Dezembro',
+    ],
+    monthNamesShort: [
+        'jan',
+        'fev',
+        'mar',
+        'abr',
+        'mai',
+        'jun',
+        'jul',
+        'ago',
+        'set',
+        'out',
+        'nov',
+        'dez',
+    ],
+    today: 'Hoje',
+    clear: 'Limpar',
+    dateFormat: 'dd/mm/yy',
+    weekHeader: 'Sm',
+}
