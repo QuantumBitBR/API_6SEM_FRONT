8de4be9e5888facc1613b2068b37d43f1faf4a67
Andre Meneses <andremeneses0103@gmail.com>
2025-11-05 22:26:21 -0300

    QB-104-feat(view): Profile View file created

---
+++ helpAi/src/views/ProfileView.vue
@@ -0,0 +1,20 @@
+<template>
+    <DefaultLayout>
+        <h1>Hello Mundo</h1>
+    </DefaultLayout>
+</template>
+
+<script>
+import DefaultLayout from '@/layouts/DefaultLayout.vue';
+
+    export default {
+        name: 'ProfileView',
+        components: {
+            DefaultLayout
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
