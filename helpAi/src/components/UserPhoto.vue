b9eb3220b0f5804dbf18f0fbf64204d767355958
Andre Meneses <andremeneses0103@gmail.com>
2025-11-06 23:57:09 -0300

    QB-104-feat(elements): created all elements in Profile View

---
+++ helpAi/src/components/UserPhoto.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="user-avatar-container">
+    <div class="user-avatar">
+      <img :src="avatarUrl" :alt="userName" />
+    </div>
+    <div class="user-info">
+      <p class="user-name">{{ userName }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserAvatar",
+  props: {
+    avatarUrl: {
+      type: String,
+      default: "https://cdn-icons-png.flaticon.com/512/847/847969.png"
+    },
+    userName: {
+      type: String,
+      default: "Usuário"
+    }
+  }
+};
+</script>
+
+<style scoped>
+.user-avatar-container {
+  display: flex;
+  align-items: center;
+  gap: 40px;
+    margin-top: 30px;
+}
+
+.user-avatar {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  overflow: hidden;
+  border: 2px solid #000;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+}
+
+.user-avatar:hover {
+  transform: scale(1.05);
+  box-shadow: 0 6px 20px rgba(0, 0, 0, 0.25);
+}
+
+.user-avatar img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.user-info {
+  flex: 1;
+}
+
+.user-name {
+  font-size: 40px;
+  font-weight: 800;
+  margin: 0;
+}
+</style>
--- helpAi/src/views/ProfileView.vue
+++ helpAi/src/views/ProfileView.vue
@@ -1,20 +1,31 @@
 <template>
     <DefaultLayout>
-        <h1>Hello Mundo</h1>
+        <div class="profile_limit">
+            <UserPhoto />
+            <UserDetails />
+        </div>
     </DefaultLayout>
 </template>
 
 <script>
+import UserDetails from '@/components/UserDetails.vue';
+import UserPhoto from '@/components/UserPhoto.vue';
 import DefaultLayout from '@/layouts/DefaultLayout.vue';
 
     export default {
         name: 'ProfileView',
         components: {
-            DefaultLayout
+            DefaultLayout,
+            UserPhoto,
+            UserDetails
         }
     }
 </script>
 
 <style scoped>
-
+.profile_limit{
+    width: 90%;
+    height: 100%;
+    margin: 0 auto;
+}
 </style>
