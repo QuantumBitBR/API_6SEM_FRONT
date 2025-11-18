9cb773a09523e01a61ff888b2054947ea995a92b
Gabriel Mota <gabmota8@gmail.com>
2025-11-17 23:15:02 -0300

    QB-112-feat(ResetEmail):Implemented requisition for sending email to reset password.

--- helpAi/src/services/UserAuthService.ts
+++ helpAi/src/services/UserAuthService.ts
@@ -51,3 +51,42 @@ export class UserAuthService {
         }
     }
 }
+
+export interface UserAuthResetPassword {
+  id: number;
+  message: string;
+}
+
+interface ResponseUserAuthResetPassword {
+  data: UserAuthResetPassword;
+}
+
+export class UserAuthResetPasswordService {
+  async resetPassword(userID: number): Promise<UserAuthResetPassword | null> {
+    try {
+      const response: AxiosResponse<ResponseUserAuthResetPassword> = await api.post(
+        `/userauth/resetar-senha`,
+        null, // Não envia body, apenas query params
+        {
+          params: {
+            user_id: userID
+          }
+        }
+      );
+      return response.data.data;
+    } catch (error: any) {
+      console.error("Error resetting password:", error);
+
+
+      if (error.response?.status === 404) {
+        throw new Error("Usuário não encontrado");
+      } else if (error.response?.status === 500) {
+        throw new Error("Erro interno ao resetar senha");
+      } else {
+        throw new Error("Erro ao resetar senha");
+      }
+    }
+  }
+}
+
+
